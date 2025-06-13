4d112ea99d41bbfaeb5db0b08b958fb4991b29ac
karthik <karthikvempala31@gmail.com>
2025-06-13 16:22:31 +0530

    Added Rate limiter logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minute
+    max: 3, // Limit each IP to 3 requests per windowMs
+    message: "Too many requests from this IP, please try again later."
+});
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(limiter);
+
+
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
